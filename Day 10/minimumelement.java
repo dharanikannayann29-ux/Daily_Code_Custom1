18293b5a75cc036629d66578d8e6ce4c89de7a5a
dharanikannayann29-ux <dharanikannayann29@gmail.com>
2026-07-25 22:54:36 -0700

    Updated Day 10 minimum element solution

--- Day 10/minimumelement.java
+++ Day 10/minimumelement.java
@@ -3,10 +3,10 @@ class Solution {
     public int minElement(int[] nums) {
         int ans = Integer.MAX_VALUE;
 
-        for(int i = 0; i < nums.length; i++) {
+        for(int i = 0; i < nums.length; i++) { 
+               
             int num = nums[i];
             int sum = 0;
-            
             while(num > 0) {
                 sum += (num % 10);
                 num /= 10;
