63ccff2511ce94fc12fe00fe96127a278340c621
dharanikannayann29-ux <dharanikannayann29@gmail.com>
2026-08-05 23:26:11 -0700

    Day 24 task completed

---
+++ Day 24/linkedlistcycle.java
@@ -0,0 +1,49 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Solution {
+
+    public boolean hasCycle(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        // Create nodes
+        ListNode n1 = new ListNode(3);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(0);
+        ListNode n4 = new ListNode(-4);
+
+        // Connect nodes
+        n1.next = n2;
+        n2.next = n3;
+        n3.next = n4;
+        n4.next = n2; // Creates a cycle
+
+        Solution obj = new Solution();
+
+        System.out.println(obj.hasCycle(n1));
+    }
+}
